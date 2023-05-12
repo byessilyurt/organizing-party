e656fd1772ecf47b8d4855209dde7e001c8de796
byessilyurt <y.yesilyurt14@gmail.com>
2023-05-13 01:48:13 +0200

    service layer

---
+++ src/main/java/yusuf_yesilyurt/organizing_party/service/EventService.java
@@ -0,0 +1,40 @@
+package yusuf_yesilyurt.organizing_party.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import yusuf_yesilyurt.organizing_party.model.Event;
+import yusuf_yesilyurt.organizing_party.repository.EventRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    @Autowired
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    public Optional<Event> getEventById(Integer id) {
+        return eventRepository.findById(id);
+    }
+
+    public List<Event> getEventsByUserId(Integer userId) {
+        return eventRepository.findByUserId(userId);
+    }
+
+    public Event saveEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    public void deleteEvent(Integer id) {
+        eventRepository.deleteById(id);
+    }
+}
